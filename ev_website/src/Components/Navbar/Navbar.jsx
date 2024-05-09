75d5191cff918f56191af3dbad688d7374e339ae
Dinusha_Madhuranga <dinushamadhuranga77@gmail.com>
2024-05-10 00:17:31 +0530

    2nd day

--- ev_website/src/Components/Navbar/Navbar.jsx
+++ ev_website/src/Components/Navbar/Navbar.jsx
@@ -2,9 +2,18 @@ import './Navbar.css'
 
 const Navbar = () => {
   return (
-    <div>
-      
-    </div>
+    
+      <div className='nav'>
+        <div className="nav-logo">EV-olution</div>
+        <ul className="nav-menu">
+            <li>Home</li>
+            <li>Explore</li>
+            <li>About</li>
+            <li className='nav-contact'>Contact</li>
+
+        </ul>
+      </div>
+    
   )
 }
 
