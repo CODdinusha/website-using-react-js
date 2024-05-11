1ae59c2fd5ff972991c12b84aa162553f4f85e69
Dinusha_Madhuranga <dinushamadhuranga77@gmail.com>
2024-05-12 01:30:52 +0530

    3rd day

--- ev_website/src/App.jsx
+++ ev_website/src/App.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react"
 import Background from "./Components/Background/Background";
+import Navbar from "./Components/Navbar/Navbar";
+import Hero from "./Components/Hero/Hero";
 
 
 const App = () => {
@@ -8,13 +10,20 @@ const App = () => {
     {text1:"Indulge",text2:"your passions"},
     {text1:"Give in to",text2:"your passions"},
   ]
-  const [heroCount,setHeroCount] = useState(0);
+  const [heroCount,setHeroCount] = useState(2);
   const [playStatus,setPlayStatus] = useState(false);
   
   return (
     <div>
       <Background playStatus={playStatus} heroCount={heroCount}/>
       <Navbar/>
+      <Hero
+        setPlayStatus={setPlayStatus}
+        heroData={heroData[heroCount]}
+        heroCount={heroCount}
+        setHeroCount={setHeroCount}
+        playStatus={playStatus}
+        />
     </div>
   )
 }
--- ev_website/src/Components/Hero/Hero.jsx
+++ ev_website/src/Components/Hero/Hero.jsx
@@ -1,9 +1,28 @@
 import './Hero.css'
-
-const Hero = () => {
+import arrow_btn from'../../assets/arr.png'
+import play_btn from'../../assets/play.png'
+import pause_btn from'../../assets/pause.png'
+const Hero = ({heroData,setHeroCount,heroCount,setPlayStatus,playStatus}) => {
   return (
-    <div>
-      
+    <div className='hero'>
+        <div className="hero-text">
+          <p>{heroData.text1}</p>
+          <p>{heroData.text2}</p>
+        </div>
+        <div className="hero-explore">
+          <p>Explore the features</p>
+          <img src={arrow_btn} alt="" />
+        </div>
+        <div className="hero-dot-play">
+          <ul className="hero-dots">
+            <li onClick={()=>setHeroCount(0)} className={heroCount===0?"hero-dot orange":"hero-dot"}></li>
+            <li onClick={()=>setHeroCount(1)} className={heroCount===1?"hero-dot orange":"hero-dot"}></li>
+            <li onClick={()=>setHeroCount(2)} className={heroCount===2?"hero-dot orange":"hero-dot"}></li>
+
+          </ul>
+          <img onClick={()=>setPlayStatus(!playStatus)} src={playStatus?pause_btn:play_btn} alt="" />
+          <p>See the video</p>
+        </div>
     </div>
   )
 }
